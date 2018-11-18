70ec21a89cd631f94dd1662320b9c35b30afc56b
Shawn Biddle <shawncplus@gmail.com>
2018-11-18 15:45:47 -0500

    don't use a state closure for attribute formula

--- attributes.js
+++ attributes.js
@@ -6,7 +6,7 @@ module.exports = [
     base: 100,
     formula: {
       requires: ['intellect'],
-      fn: state => function (character, mana, intellect) {
+      fn: function (character, mana, intellect) {
         return mana + (intellect * 10);
       }
     },
